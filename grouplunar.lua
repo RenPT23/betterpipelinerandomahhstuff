74f36ee2e6e295f7e17c13879c6dbef96bf6b2e0
RenPT23 <161973086+RenPT23@users.noreply.github.com>
2026-07-24 10:21:15 +0100

    Refactor image loading and rendering in grouplunar.lua

--- grouplunar.lua
+++ grouplunar.lua
@@ -1,74 +1,50 @@
 local gpu = peripheral.wrap("top")
 gpu.refreshSize()
 gpu.setSize(16)
-
 local gl = gpu.createWindow3D(0, 0, 48, 48)
-
 gl.glFrustum(90, 1, 4)
 gl.glDirLight(0, 0, -1)
-
 local f = io.open("grouplunar.png", "rb")
-local data = f:read("*a")
+local bytes = {}
+local b = f._handle.read(1)
+while b do
+    bytes[#bytes + 1] = ("<I1"):unpack(b)
+    b = f._handle.read(1)
+end
 f:close()
-
-local bytes = { data:byte(1, #data) }
 local image = gpu.decodeImage(table.unpack(bytes))
-
 local tex = gl.glGenTextures()
 gl.glBindTexture(tex)
 gl.glTexImage(image.ref())
-
 local glc = {}
 local c = { gl.getConstants() }
 for i = 1, #c, 2 do
     glc[c[i]] = c[i + 1]
 end
-
-for k, v in pairs(glc) do
-    print(tostring(k), tostring(v))
-end
-
-gl.glEnable(3553)
-
--- quad distance from camera and FOV-matched half-extent so it fills the screen
-local dist = 3
-local halfSize = dist * math.tan(math.rad(90 / 2)) -- 90 fov -> tan(45) = 1, so halfSize = dist
-
+gl.glEnable(glc.GL_TEXTURE_2D)
 while true do
     gl.clear()
-
     gl.glLoadIdentity()
-    gl.glTranslate(0, 0, dist)
-
+    gl.glTranslate(0, 0, 3)
     gl.glColor(255, 255, 255)
-
-    gl.glBegin(4) -- GL_TRIANGLES
-
+    gl.glBegin(glc.GL_TRIANGLES)
     -- Triangle 1
     gl.glTexCoord(0, 0)
-    gl.glVertex(-halfSize, -halfSize, 0)
-
+    gl.glVertex(-3, -3, 0)
     gl.glTexCoord(0, 1)
-    gl.glVertex(-halfSize,  halfSize, 0)
-
+    gl.glVertex(-3, 3, 0)
     gl.glTexCoord(1, 1)
-    gl.glVertex( halfSize,  halfSize, 0)
-
+    gl.glVertex(3, 3, 0)
     -- Triangle 2
     gl.glTexCoord(0, 0)
-    gl.glVertex(-halfSize, -halfSize, 0)
-
+    gl.glVertex(-3, -3, 0)
     gl.glTexCoord(1, 1)
-    gl.glVertex( halfSize,  halfSize, 0)
-
+    gl.glVertex(3, 3, 0)
     gl.glTexCoord(1, 0)
-    gl.glVertex( halfSize, -halfSize, 0)
-
+    gl.glVertex(3, -3, 0)
     gl.glEnd()
-
     gl.render()
     gl.sync()
     gpu.sync()
-
     sleep(0)
 end
