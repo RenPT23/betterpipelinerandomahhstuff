458576d9d9559f7f1abb627b7e36049cd261a36c
RenPT23 <161973086+RenPT23@users.noreply.github.com>
2026-07-24 19:25:29 +0100

    Fix image size calculation for window creation
    Adjusted image dimensions by subtracting 2 pixels for display.

--- imgloader.lua
+++ imgloader.lua
@@ -13,13 +13,13 @@ local bytes = { data:byte(1, #data) }
 local image = gpu.decodeImage(table.unpack(bytes))
 
 local imgW, imgH = image.getWidth(), image.getHeight()
-print("image size: " .. imgW .. "x" .. imgH)
+print("image size: " .. imgW-2 .. "x" .. imgH-2)
 
 gpu.fill()
 gpu.sync()
 
 local scale = 1 -- this needs to be modified based on the image
-local win = gpu.createWindow(1, 1, imgW * scale, imgH * scale)
+local win = gpu.createWindow(1, 1, imgW * scale - 2, imgH * scale - 2)
 
 for y = 0, imgH - 3 do
     local sampleY = y
